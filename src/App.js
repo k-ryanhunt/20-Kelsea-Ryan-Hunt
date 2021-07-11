2de65e205aa9666cda3faa73431d0cf18cc693f1
K Ryan <hunt.kelsearyan@gmail.com>
2021-07-11 13:56:00 -0600

    More file structures and using template from activities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Wrapper from "./components/wrapper/wrapper";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/navBar/navBar";
+import About from "./pages/aboutMe";
+import Portfolio from "./pages/workPortfolio";
+import Contact from "./pages/contactMe";
+import NotFoundPage from "./pages/notFound";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Wrapper>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route component={NotFoundPage} />{" "}
+        </Switch>
+      </Wrapper>
+    </Router>
   );
 }
 
---
+++ src/pages/notFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div>
+      <h2>404 Error: Not Found</h2>
+    </div>
+  );
+};
+
+export default notFound;
